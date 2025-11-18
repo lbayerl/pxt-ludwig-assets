7cd271e0250ed574fa697dd169b4a0c9f19ac10d
L Bayerl <lugge@lugge.de>
2025-11-18 14:26:27 +0100

    Aktualisiere main.py, images.g.jres, images.g.ts

--- main.py
+++ main.py
@@ -2,32 +2,37 @@
 # % block="Ludwig Assets"
 @namespace
 class ludwigAssets:
-    # % blockId="ludwig_testSpriteImg_py"
+    # % blockId="ludwig_testSpriteImg"
     # % block="Test-Sprite Bild"
     # % imageLiteral=1
     def testSpriteImg():
-        return assets.image("lalien")
+        # return the image asset from the JRES / generated factory
+        return assets.image("""
+            lalien
+            """)
     # 
     # 2) Kleiner Hit-Soundeffekt
     # 
-    # % blockId="ludwig_playTestSfx_py"
+    # % blockId="ludwig_playTestSfx"
     # % block="spiele Test-SFX"
     def playTestSfx():
-        sfx = music.create_sound_effect(WaveShape.NOISE,
-            500,
-            100,
+        sfx = music.create_sound_effect(WaveShape.SINE,
+            5000,
+            0,
             255,
             0,
-            200,
+            500,
             SoundExpressionEffect.NONE,
-            InterpolationCurve.CURVE)
+            InterpolationCurve.LINEAR)
         music.play(sfx, music.PlaybackMode.IN_BACKGROUND)
     # 
     # 3) Kleine Test-Musik
+    # (switch to playMelody for reliability in the block UI/runtime)
     # 
-    # % blockId="ludwig_playTestMusic_py"
+    # % blockId="ludwig_playTestMusic"
     # % block="spiele Test-Musik"
     def playTestMusic():
+        # a simple melody that's reliable and visible in the blocks UI
         music.play_melody("C D E F G A B C5", 120)
     # 
     # 4) Einfaches Tile + Tilemap
@@ -50,7 +55,7 @@ class ludwigAssets:
         6 . . . . . . . . . . . . . . 6
         6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6
         """)
-    # % blockId="ludwig_setTestTilemap_py"
+    # % blockId="ludwig_setTestTilemap"
     # % block="setze Test-Tilemap"
     def setTestTilemap():
         tm = tiles.create_tilemap(hex("""
